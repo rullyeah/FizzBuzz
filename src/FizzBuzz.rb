54ed393a2af3ef3f4164029da145645740127160
Raúl Nadal <rullyeah@gmail.com>
2017-12-19 22:47:21 +0100

    Test: return Fizz when n contains 3: Refactor

--- src/FizzBuzz.rb
+++ src/FizzBuzz.rb
@@ -5,22 +5,30 @@ class FizzBuzz
     BUZZ_NUMBER = 5
 
     def says n
-      return 'FizzBuzz' if checkFizzBuzz n
-      return 'Fizz' if checkFizz n
-      return 'Buzz' if checkBuzz n
+      return 'FizzBuzz' if checkFizzBuzz? n
+      return 'Fizz' if checkFizz? n
+      return 'Buzz' if checkBuzz? n
       n
     end
 
-    def checkFizz n
-      n % FIZZ_NUMBER == 0 or  n.digits.member? FIZZ_NUMBER
+    def checkFizz? n
+      is_divisible? n, FIZZ_NUMBER or contains? n, FIZZ_NUMBER
     end
 
-    def checkBuzz n
+    def checkBuzz? n
       n % BUZZ_NUMBER == 0
     end
 
-    def checkFizzBuzz n
-      checkFizz n and checkBuzz n
+    def checkFizzBuzz? n
+      checkFizz? n and checkBuzz? n
+    end
+
+    def is_divisible? n, divisor
+      n % divisor == 0
+    end 
+
+    def contains? n, pattern
+      n.digits.member? pattern
     end
   end
 end
