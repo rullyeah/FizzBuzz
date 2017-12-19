ed2ecffa04b0185cf450eb4689f75eb17e1d5a09
Raúl Nadal <rullyeah@gmail.com>
2017-12-19 21:30:15 +0100

    Test: return Buzz with 5: Refactor

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,23 +1,25 @@
 describe 'FizzBuzz' do
-  it 'return fizz when the number is 3' do
-    result = fizzBuzz 3
-    expect('Fizz').to eq(result)
-  end
-
   it 'return the same number by default' do
     result = fizzBuzz 1
     expect(1).to eq(result)
+  end
+
+  it 'return Fizz when the number is 3' do
+    result = fizzBuzz 3
+    expect('Fizz').to eq(result)
   end
 
   it 'return Buzz when the number is 5' do
     result = fizzBuzz 5
     expect('Buzz').to eq(result)
   end
-
 end
 
+FIZZ_NUMBER = 3
+BUZZ_NUMBER = 5
+
 def fizzBuzz n
-  return 'Fizz' if n == 3
-  return 'Buzz' if n == 5
+  return 'Fizz' if n == FIZZ_NUMBER
+  return 'Buzz' if n == BUZZ_NUMBER
   n
 end
