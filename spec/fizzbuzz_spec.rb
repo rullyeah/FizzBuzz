71e92f7e3d9e3c3da6f6d5f1920b45ae7964d498
Raúl Nadal <rullyeah@gmail.com>
2017-12-19 21:18:54 +0100

    Test: return Fizz with 3: Green

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,10 @@
 describe 'FizzBuzz' do
   it 'return fizz when the number is 3' do
-  	result = fizzBuzz 3
+    result = fizzBuzz 3
     expect('Fizz').to eq(result)
   end
 end
 
 def fizzBuzz n
+  'Fizz'
 end
