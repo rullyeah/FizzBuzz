4bc57b600c71a8f00b8cf69dd43fae27af5922ba
Raúl Nadal <rullyeah@gmail.com>
2017-12-19 22:31:50 +0100

    Test: return Fizz when n contains 3: Red

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,21 +3,26 @@ require_relative '../src/FizzBuzz'
 describe 'FizzBuzz' do
   it 'return the same number by default' do
     result = FizzBuzz.says 1
-    expect(1).to eq(result)
+    expect(result).to eq(1)
   end
 
   it 'return Fizz when the number is divisible by 3' do
     result = FizzBuzz.says 9
-    expect('Fizz').to eq(result)
+    expect(result).to eq('Fizz')
   end
 
   it 'return Buzz when the number is divisible by 5' do
     result = FizzBuzz.says 10
-    expect('Buzz').to eq(result)
+    expect(result).to eq('Buzz')
   end
 
   it 'return FizzBuzz when the number is divisible by 3 and 5' do
     result = FizzBuzz.says 30
-    expect('FizzBuzz').to eq(result)
+    expect(result).to eq('FizzBuzz')
+  end
+
+  it 'return Fizz when the number contains 3' do
+    result = FizzBuzz.says 13
+    expect(result).to eq('Fizz')
   end
 end
